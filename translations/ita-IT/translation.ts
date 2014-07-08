8dba771dd9b147c1e042bc7317a645fab30fffd2
Luca Realdi <luca.realdi@opencontent.it>
2014-07-08 12:13:30 +0200

    Add translation

---
+++ translations/ita-IT/translation.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>survey</name>
+    <message>
+        <source>You need to log in in order to answer this survey</source>
+        <translation type="unfinished">Devi essere autenticato per poter rispondere al questionario</translation>
+    </message>
+    <message>
+        <source>The survey is not active</source>
+        <translation type="unfinished">Il questionario non è attivo</translation>
+    </message>
+    <message>
+        <source>The survey does already have an answer from you</source>
+        <translation type="unfinished">Hai già risposto a questo questionario</translation>
+    </message>
+    <message>
+        <source>Questions marked with %mark% are required.</source>
+        <translation type="unfinished">Sono obbligatore le risposte alle domande contrassegnate da %mark%.</translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation type="unfinished">Invia</translation>
+    </message>
+    <message>
+        <source>Text of question</source>
+        <translation type="unfinished">Testo della domanda</translation>
+    </message>
+    <message>
+        <source>Mandatory answer</source>
+        <translation type="unfinished">Risposta obbligatoria</translation>
+    </message>
+    <message>
+        <source>Question Matrix</source>
+        <translation type="unfinished">Matrice di domande</translation>
+    </message>
+    <message>
+        <source>Number of columns for the matrix</source>
+        <translation type="unfinished">Numero di colonne</translation>
+    </message>
+    <message>
+        <source>Number of rows for the matrix</source>
+        <translation type="unfinished">Numero di righe</translation>
+    </message>
+    <message>
+        <source>Page Break</source>
+        <translation type="unfinished">Interruzione di pagina</translation>
+    </message>
+    <message>
+        <source>Number of columns for an answer textarea</source>
+        <translation type="unfinished">Numero di colonne del campo di testo</translation>
+    </message>
+    <message>
+        <source>Number of rows</source>
+        <translation type="unfinished">Numero di righe</translation>
+    </message>
+    <message>
+        <source>Code (separated by commas)</source>
+        <translation type="unfinished">Codice (separato da virgola)</translation>
+    </message>
+    <message>
+        <source>Text entry (Validated)</source>
+        <translation type="unfinished">Testo validato</translation>
+    </message>
+    <message>
+        <source>Validation Type</source>
+        <translation type="unfinished">Tipo di validazione</translation>
+    </message>
+    <message>
+        <source>Default settings</source>
+        <translation type="unfinished">Configurazione predefinita</translation>
+    </message>
+    <message>
+        <source>Default answer</source>
+        <translation type="unfinished">Risposta predefinita</translation>
+    </message>
+    <message>
+        <source>User email</source>
+        <translation type="unfinished">Email dell&apos;utente</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation type="unfinished">Nome utente</translation>
+    </message>
+    <message>
+        <source>Last answers</source>
+        <translation type="unfinished">Ultime risposte</translation>
+    </message>
+    <message>
+        <location filename="ezsurveycountry.php" line="62"/>
+        <source>Please select a country in question %number</source>
+        <translation type="unfinished">Seleziona un paese nella domanda %number</translation>
+    </message>
+    <message>
+        <location filename="ezsurveycountry.php" line="96"/>
+        <source>Country</source>
+        <translation type="unfinished">Paese</translation>
+    </message>
+    <message>
+        <location filename="ezsurveyrsvpcode.php" line="28"/>
+        <location filename="ezsurveytextentryvalidated.php" line="37"/>
+        <source>Please answer the question %number as well!</source>
+        <translation type="unfinished">Rispondi alla domanda %number!</translation>
+    </message>
+    <message>
+        <location filename="ezsurveyrsvpcode.php" line="51"/>
+        <source>The RSVP code is not correct</source>
+        <translation type="unfinished">Il codice RSVP non è corretto</translation>
+    </message>
+    <message>
+        <location filename="ezsurveyrsvpcode.php" line="82"/>
+        <source>RSVP Code</source>
+        <translation type="unfinished">Codice RSVP</translation>
+    </message>
+    <message>
+        <location filename="ezsurveystate.php" line="69"/>
+        <source>Please select a state or select &quot;Other State&quot; and type in a state in question %number</source>
+        <translation type="unfinished">Seleziona uno stato oppure scrivi uno stato nel campo &quot;Altro&quot; nella domanda %number</translation>
+    </message>
+    <message>
+        <location filename="ezsurveystate.php" line="112"/>
+        <source>State</source>
+        <translation type="unfinished">Stato</translation>
+    </message>
+    <message>
+        <location filename="ezsurveymatrix.php" line="105"/>
+        <source>Please mark all the possible choices in question %number</source>
+        <translation type="unfinished">Seleziona tutte le risposte nella domanda %number</translation>
+    </message>
+    <message>
+        <location filename="ezsurveymatrix.php" line="140"/>
+        <source>Matrix</source>
+        <translation type="unfinished">Matrice</translation>
+    </message>
+    <message>
+        <location filename="ezsurveypagebreak.php" line="26"/>
+        <source>Page break</source>
+        <translation type="unfinished">Interruzione di pagina</translation>
+    </message>
+    <message>
+        <location filename="ezsurveytextentryvalidated.php" line="67"/>
+        <source>Text Entry (Validated)</source>
+        <translation type="unfinished">Testo validato</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/untranslated/translation.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>survey</name>
+    <message>
+        <source>You need to log in in order to answer this survey</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The survey is not active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The survey does already have an answer from you</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Questions marked with %mark% are required.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text of question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mandatory answer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Question Matrix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of columns for the matrix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of rows for the matrix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Page Break</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of columns for an answer textarea</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of rows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Code (separated by commas)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text entry (Validated)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Validation Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default answer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last answers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveycountry.php" line="62"/>
+        <source>Please select a country in question %number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveycountry.php" line="96"/>
+        <source>Country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveyrsvpcode.php" line="28"/>
+        <location filename="ezsurveytextentryvalidated.php" line="37"/>
+        <source>Please answer the question %number as well!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveyrsvpcode.php" line="51"/>
+        <source>The RSVP code is not correct</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveyrsvpcode.php" line="82"/>
+        <source>RSVP Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveystate.php" line="69"/>
+        <source>Please select a state or select &quot;Other State&quot; and type in a state in question %number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveystate.php" line="112"/>
+        <source>State</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveymatrix.php" line="105"/>
+        <source>Please mark all the possible choices in question %number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveymatrix.php" line="140"/>
+        <source>Matrix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveypagebreak.php" line="26"/>
+        <source>Page break</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezsurveytextentryvalidated.php" line="67"/>
+        <source>Text Entry (Validated)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
